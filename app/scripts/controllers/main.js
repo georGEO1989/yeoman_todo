551eefae3197d194ebe3767d6e43e7096edba972
georGEO1989 <goffley3@aol.com>
2013-12-16 23:40:04 -0500

    did everything uo to the local strage, issue with array

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('yoTodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+   // $scope.todos = ['Item 1', 'Item2', 'Item3'];
+
+   var todosInStore = localStorageService.get('todos');
+   $scope.todos = todosInStore && todosInStore.split('\n') || [];
+   $scope.$watch(function(){
+   		localStorageService.add('todos', $scope.todos.join('\n'));
+   });
+
+    $scope.addTodo = function(){
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    }
+
+    $scope.removeTodo = function(index){
+    	$scope.todos.splice(index, 1);
+    }
+  });
